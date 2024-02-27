d73316e71339393eaced6ce242d19131e5cee53c
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-02-27 13:22:13 -0600

    pay rent functionality works now

--- src/Components/UI Components/ModifyBudgetModal.jsx
+++ src/Components/UI Components/ModifyBudgetModal.jsx
@@ -1,11 +1,72 @@
-export default function ModifyBudgetModal({id}){
+import { Button } from "@nextui-org/react"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router"
 
-    return(
+export default function ModifyBudgetModal({ id, data }) {
+    const pageID = useParams()
+    const [allData, setAllData] = useState()
+    console.log('pageid', pageID)
+    const URL = `https://api.ttguitarnoob.cloud/budgets/${pageID.id}`
+
+    async function handleFetch(){
+        const options = {
+            method: "GET"
+        }
+
+        try{
+            const response = await fetch(URL, options)
+            const data = await response.json()
+            setAllData(data)
+            console.log("fetched the data from the modal", data)
+            return data
+
+        } catch(err) {
+            console.log("omg another fetch error from the edit budget item modal thing", err)
+        }
+    }
+
+    //Pay Bill
+    function payBill(){
+        allData.allData.bills[id].paid = true
+        handlePut()
+    }
+
+    //Make the put request
+    async function handlePut(){
+        console.log('putting it where it belongs', allData.allData)
+        const options = {
+            method: "PUT",
+            body: JSON.stringify(allData.allData),
+            mode: "cors",
+            headers: {
+                "Content-type": "application/json"
+            }
+        }
+        try{
+            const updatedItem = await fetch(URL, options)
+            
+        } catch(err) {
+            console.log("you broke the put method bro from updating a budget item", err)
+        }
+
+    }
+
+     //Call handleFetch
+     useEffect(() => {
+        handleFetch()
+    }, [])
+
+
+
+
+    return (
         <>
-        <section>
-            <h1>Modify This Budget Item</h1>
-            <p>{id}</p>
-        </section>
+            <section>
+                <h1>Modify This Budget Item</h1>
+                <p>{id}</p>
+                <p>{pageID.id}</p>
+                <Button onClick={payBill}>Pay</Button>
+            </section>
         </>
     )
 }
