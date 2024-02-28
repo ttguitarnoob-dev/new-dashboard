986305815a7c955fc6fae616be6b2b431c81cfae
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-02-28 15:38:57 -0600

    made budget page look better

--- src/Components/Pages/Budgety Thing/Budget.jsx
+++ src/Components/Pages/Budgety Thing/Budget.jsx
@@ -1,4 +1,4 @@
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Button} from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Button } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
@@ -17,54 +17,55 @@ const columns = [
 
 export default function Budget() {
 
-    const navigate = useNavigate()
-    const [budgets, setBudgets] = useState({})
-    
+  const navigate = useNavigate()
+  const [budgets, setBudgets] = useState({})
 
-    function handleClick(url) {
-        navigate(url)
+
+  function handleClick(url) {
+    navigate(url)
+  }
+
+  async function handleFetch() {
+    const URL = "https://api.ttguitarnoob.cloud/budgets"
+    // const URL = "http://localhost:8000/budgets"
+    const options = {
+      method: "GET"
     }
 
-    async function handleFetch(){
-        const URL = "https://api.ttguitarnoob.cloud/budgets"
-        // const URL = "http://localhost:8000/budgets"
-        const options = {
-            method: "GET"
-        }
-
-        try{
-            const response = await fetch(URL, options)
-            const data = await response.json()
-            rows = data
-            setBudgets(data)
-        } catch(err){
-            console.log("shit happened when fetching that", err)
-        }
+    try {
+      const response = await fetch(URL, options)
+      const data = await response.json()
+      rows = data
+      setBudgets(data)
+    } catch (err) {
+      console.log("shit happened when fetching that", err)
     }
+  }
 
-    //Call handleFetch
-    useEffect(() => {
-        handleFetch()
-    }, [])
+  //Call handleFetch
+  useEffect(() => {
+    handleFetch()
+  }, [])
 
   return (
     <>
 
-
-    <h1 className="page-title">Budgety Thing</h1>
-                <Button onClick={() => handleClick("/budget/new")}>Add Month</Button>
-    <Table aria-label="Example table with dynamic content">
-      <TableHeader columns={columns}>
-        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody items={rows}>
-        {(item) => (
-          <TableRow key={item.key}>
-            {(columnKey) => <TableCell><Button onClick={() => handleClick(`/budget/${item.key}`)}>{getKeyValue(item, columnKey)}</Button> </TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+      <section className="p-10">
+      <h1 className="page-title">Budgety Thing</h1>
+      <Button  onClick={() => handleClick("/budget/new")}>Add Month</Button>
+      </section>
+      <Table aria-label="Example table with dynamic content">
+        <TableHeader columns={columns}>
+          {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+        </TableHeader>
+        <TableBody items={rows}>
+          {(item) => (
+            <TableRow key={item.key}>
+              {(columnKey) => <TableCell><Button onClick={() => handleClick(`/budget/${item.key}`)}>{getKeyValue(item, columnKey)}</Button> </TableCell>}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
     </>
   );
 }
