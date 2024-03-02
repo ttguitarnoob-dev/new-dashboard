369383e4b6ac90ebfc6987c39f05b6e153e2d4a5
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-03-02 10:43:36 -0600

    added edit expense button

--- src/Components/UI Components/ModifyBudgetModal.jsx
+++ src/Components/UI Components/ModifyBudgetModal.jsx
@@ -6,41 +6,43 @@ import { DollarIcon } from "./SVG Icons/DollarIcon"
 export default function ModifyBudgetModal({ id, data }) {
     const pageID = useParams()
     const [allData, setAllData] = useState()
-    console.log('pageid', pageID)
     const URL = `https://api.ttguitarnoob.cloud/budgets/${pageID.id}`
     const putURL = `https://api.ttguitarnoob.cloud/budgets/update-unpaid/${pageID.id}`
 
-    async function handleFetch(){
+    async function handleFetch() {
         const options = {
             method: "GET"
         }
 
-        try{
+        try {
             const response = await fetch(URL, options)
             const data = await response.json()
             setAllData(data)
-            console.log("fetched the data from the modal", data)
             return data
 
-        } catch(err) {
+        } catch (err) {
             console.log("omg another fetch error from the edit budget item modal thing", err)
         }
     }
 
     //Pay Bill
-    function payBill(){
+    function payBill() {
         const theBill = allData.allData.bills[id].paid
-        if (!theBill){
+        if (!theBill) {
             allData.allData.bills[id].paid = true
-        }else {
+        } else {
             allData.allData.bills[id].paid = false
         }
         handlePut()
     }
 
+    //Edit the expense details
+    function handleEdit() {
+        console.log("editing the stuff")
+    }
+
     //Make the put request
-    async function handlePut(){
-        console.log('putting it where it belongs', allData.allData)
+    async function handlePut() {
         const options = {
             method: "PUT",
             body: JSON.stringify(allData.allData),
@@ -49,29 +51,39 @@ export default function ModifyBudgetModal({ id, data }) {
                 "Content-type": "application/json"
             }
         }
-        try{
+        try {
             const updatedItem = await fetch(putURL, options)
             window.location.reload()
-        } catch(err) {
+        } catch (err) {
             console.log("you broke the put method bro from updating a budget item", err)
         }
 
     }
 
-     //Call handleFetch
-     useEffect(() => {
+    //Call handleFetch
+    useEffect(() => {
         handleFetch()
     }, [])
 
 
-
+    if (!allData) {
+        return (
+            <>
+                <section>
+                    <h1 className="text-2xl">Loading Data...</h1>
+                </section>
+            </>
+        )
+    }
 
     return (
         <>
             <section>
-                <h1 className="text-2xl">Modify This Budget Item</h1>
-                
-                <Button startContent={<DollarIcon/>} className="mt-10" onClick={payBill}>Update Paid Status</Button>
+                <h1 className="text-2xl">Modify {allData.allData.bills[id].billName}</h1>
+                <div className="flex">
+                    <Button startContent={<DollarIcon />} className="mt-10" onClick={payBill}>Update Paid Status</Button>
+                    <Button startContent={<DollarIcon />} className="ml-2 mt-10" onClick={handleEdit}>Edit {allData.allData.bills[id].billName}</Button>
+                </div>
             </section>
         </>
     )
--- src/Components/UI Components/SVG Icons/DollarIcon.jsx
+++ src/Components/UI Components/SVG Icons/DollarIcon.jsx
@@ -9,7 +9,7 @@ export const DollarIcon = ({
     ...props
 }) => {
     return (
-        <svg width={size || width || 24} height={size || height || 24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width={size || width || 30} height={size || height || 30} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g
                 fill="none"
                 stroke={fill}
