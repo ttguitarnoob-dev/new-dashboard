d1df7449e91ddcc0c822e31d8c214f5e892fbab7
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-03-09 10:57:36 -0600

    adding invoices adds invoice id to job now

--- src/Components/Pages/Tombstone Stuff/CloseJob.jsx
+++ src/Components/Pages/Tombstone Stuff/CloseJob.jsx
@@ -8,12 +8,16 @@
 //submit button will trigger its own function that has if generate = true, then run its function, etc
 
 import { Button, Checkbox, CheckboxGroup } from "@nextui-org/react"
+import { useState } from "react"
+import { customersColumns } from "../../../utils/tableData"
 
 export default function CloseJob({ customerData, jobIndex }) {
-    
+
     let isChecked = [false, false, false]
+    let hasInvoice = checkForInvoice()
+    const customerID = customerData._id
     const invoiceURL = `http://localhost:8000/invoices`
-    const customerURL = `http://localhost:8000/customers${customerData._id}`
+    const customerURL = `http://localhost:8000/customers/invoice-id/${customerID}`
     const invoiceData = {
         client: customerData.name,
         phone: customerData.phone,
@@ -29,6 +33,19 @@ export default function CloseJob({ customerData, jobIndex }) {
 
     }
 
+    //Determine if job already has invoice
+    function checkForInvoice() {
+        console.log('customerdata from invoicchecker', customerData.jobs[jobIndex])
+        const invoice = customerData.jobs[jobIndex].invoiceID
+        console.log('hasinvoice', invoice)
+        if (invoice === null) {
+            return false
+        } else {
+            return true
+        }
+
+    }
+
     function checkTheChecks(index) {
         if (!isChecked[index]) {
             isChecked[index] = true
@@ -36,10 +53,10 @@ export default function CloseJob({ customerData, jobIndex }) {
             isChecked[index] = false
         }
     }
-    console.log('customerdata', invoiceData)
 
     //Generate invoice, linked to isChecked[0]
     async function generateInvoice() {
+        
         console.log('yo im generating an invoice now', customerData._id, jobIndex)
         const newInvoiceOptions = {
             method: "POST",
@@ -48,21 +65,43 @@ export default function CloseJob({ customerData, jobIndex }) {
                 "Content-type": "application/json"
             }
         }
+
+
         try {
             const response = await fetch(invoiceURL, newInvoiceOptions)
             const data = await response.json()
-            const invoiceId = data.invoiceId
-            console.log('the data', data.invoiceId)
-            console.log(`https://majestic-monuments.ttguitarnoob.cloud/invoice/${invoiceId}`)
+            const updateCustomerData = {
+                customerID: customerID,
+                jobIndex: jobIndex,
+                invoiceID: data.invoiceId
+            }
+            const updateOptions = {
+                method: "PUT",
+                body: JSON.stringify(updateCustomerData),
+                mode: "cors",
+                headers: {
+                    "Content-type": "application/json"
+                }
+            }
+            console.log('sending put request to ', customerURL)
+            const putRequest = await fetch(customerURL, updateOptions)
+            const putData = await putRequest.json()
+
 
         } catch (err) {
             console.log('ya you really went too far this time', err)
         }
+
+        // try {
+
+        // } catch(err) {
+        //     console.log('second try errr', err)
+        // }
     }
 
     //Send Email, linked to isChecked[1]
     async function sendEmail() {
-        console.log('sending email now', customerData)
+        console.log('sending email now', customerData.email)
     }
 
     //Update paid, linked to isChecked[2]
@@ -94,12 +133,13 @@ export default function CloseJob({ customerData, jobIndex }) {
     return (
         <>
             <section>
-                <CheckboxGroup label="Select An Action">
-                    <Checkbox value={'invoice'} onChange={() => checkTheChecks(0)}>Generate Invoice</Checkbox>
+                <CheckboxGroup className="mt-5" label="Select An Action">
+                    {!hasInvoice && <Checkbox value={'invoice'} onChange={() => checkTheChecks(0)}>Generate Invoice</Checkbox>}
+                    {hasInvoice && <a style={{ textDecoration: 'underline' }} href={`https://majestic-monuments.ttguitarnoob.cloud/invoices/345345`} target="_blank">Link to Invoice</a>}
                     <Checkbox value={'email'} onChange={() => checkTheChecks(1)}>Send Invoice Email</Checkbox>
                     <Checkbox value={'paid'} onChange={() => checkTheChecks(2)}>Update Paid</Checkbox>
                 </CheckboxGroup>
-                <Button onClick={handleSubmit}>Submit</Button>
+                <Button className="mt-5" onClick={handleSubmit}>Submit</Button>
             </section>
         </>
     )
