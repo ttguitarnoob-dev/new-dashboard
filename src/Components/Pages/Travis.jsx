684e9ed924c5fa4ba27bc63ee2735bcecb69077f
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-04-20 10:01:01 -0500

    corrected links and added another

--- src/Components/Pages/Travis.jsx
+++ src/Components/Pages/Travis.jsx
@@ -2,7 +2,8 @@ import ButtonList from "../UI Components/ButtonList"
 
 export default function Travis() {
     const itemList = [
-        { text: "Proxmox", link: "https://proxmox.ttguitarnoob.cloud", tab: true },
+        { text: "Proxmox", link: "https://proxmox.travisty-creations.com", tab: true },
+        { text: "Guac", link: "https://guacamole.travisty-creations.com", tab: true },
         { text: "PiHole", link: "http://10.24.24.112/admin/login.php", tab: true },
         { text: "Schedule", link: "https://docs.google.com/spreadsheets/d/1naOVRBDOi6G_Amtr8U06ITkPpGE9vQC9/edit?usp=sharing&ouid=116033080101945418210&rtpof=true&sd=true", tab: true },
         { text: "Budget", link: "/budget", tab: false },
