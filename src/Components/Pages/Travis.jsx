e3151b14bea3c2f3d014ed0ece69f32fbff259c5
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-04-26 12:34:53 -0500

    added pfsesne buton

--- src/Components/Pages/Travis.jsx
+++ src/Components/Pages/Travis.jsx
@@ -5,6 +5,7 @@ export default function Travis() {
         { text: "Proxmox", link: "https://proxmox.travisty-creations.com", tab: true },
         { text: "Guac", link: "https://guacamole.travisty-creations.com", tab: true },
         { text: "PiHole", link: "http://10.24.24.112/admin/login.php", tab: true },
+        { text: "PfSense", link: "https://10.24.24.1", tab: true },
         { text: "Schedule", link: "https://docs.google.com/spreadsheets/d/1naOVRBDOi6G_Amtr8U06ITkPpGE9vQC9/edit?usp=sharing&ouid=116033080101945418210&rtpof=true&sd=true", tab: true },
         { text: "Budget", link: "/budget", tab: false },
 
