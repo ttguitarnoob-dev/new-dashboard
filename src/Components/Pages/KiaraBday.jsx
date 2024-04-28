adbc9a72949e9fa72dd1eaf4ed5216c0a664f67b
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-04-28 09:31:14 -0500

    more bdya

--- src/Components/Pages/KiaraBday.jsx
+++ src/Components/Pages/KiaraBday.jsx
@@ -5,7 +5,7 @@ export default function KiaraBday() {
 
     const [numero, setNumero] = useState(0)
 
-    const words = ["Hi, welcome to your 29th year!", <Image src="http://3.bp.blogspot.com/-0Yeu1BlYjiQ/Vyvr8jERoiI/AAAAAAAAAoo/e5QMuGxWUrwpU0gHx1gz8yjXOnB-BAhOACHM/s1600/kitten-cute-kitty-cat.jpg"/>, "When I first met you I thought it was so attractive that you were still in your youth", "At 24, you brought out a part of my life that I thought I'd missed", "Now that you're almost out of your 20s, you may not seem as youthful as you were when we first met", "But you're even more you than you ever were", "You've been growing in wisdom since we met, and now you're more attractive than ever.", "So this year, hold on to your youth and cultivate your wisdom!", "Life starts for real at 30!", "LOVE YOU HAPPY BIRTHDAY!", <Button as={Link} href="/normalkiara"><a>Go to your normal dashboard!</a></Button>]
+    const words = ["Hi, welcome to your 29th year!", <Image src="http://3.bp.blogspot.com/-0Yeu1BlYjiQ/Vyvr8jERoiI/AAAAAAAAAoo/e5QMuGxWUrwpU0gHx1gz8yjXOnB-BAhOACHM/s1600/kitten-cute-kitty-cat.jpg"/>, "Random kitty picture for no reason", "When I first met you I thought it was so attractive that you were still in your youth", "At 24, you brought out a part of my life that I thought I'd missed", "Now that you're almost out of your 20s, you may not seem as youthful as you were when we first met", "But you're even more you than you ever were", "You've been growing in wisdom since we met, and now you're more attractive than ever.", "So this year, hold on to your youth and cultivate your wisdom!", "Life starts for real at 30!", "LOVE YOU HAPPY BIRTHDAY!", <Button as={Link} href="/normalkiara"><a>Go to your normal dashboard!</a></Button>]
     let number = 1
 
     return(
