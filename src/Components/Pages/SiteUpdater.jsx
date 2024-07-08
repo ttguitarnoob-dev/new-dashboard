9d44a74dc96b06edb88eb89c09c6803c794ff09d
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-07-08 09:35:07 -0500

    started site updater

---
+++ src/Components/Pages/SiteUpdater.jsx
@@ -0,0 +1,24 @@
+import { Button, Image, Link } from "@nextui-org/react"
+import { useState } from "react"
+
+export default function SiteUpdater() {
+
+    // fetch route that scans each folder in the site directory and displays an update button for each one
+
+    // fetch route that updates the folder based on the button selected
+    
+
+    return(
+        <>
+        <section>
+            <h1 className="page">Site Updater!</h1>
+            <div className="page mt-10">
+                {words[numero]}
+            </div>
+            <div className="page mt-10">
+                <Button onClick={() => setNumero(numero + 1)}>Next</Button>
+            </div>
+        </section>
+        </>
+    )
+}
