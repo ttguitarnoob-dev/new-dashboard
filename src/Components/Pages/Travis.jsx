bf96116c651d3377635b5be343aefebb98a064d7
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-07-16 11:47:29 -0500

    added site updater to travis list

--- src/Components/Pages/Travis.jsx
+++ src/Components/Pages/Travis.jsx
@@ -8,6 +8,7 @@ export default function Travis() {
         { text: "PfSense", link: "https://10.24.24.1", tab: true },
         { text: "Schedule", link: "https://docs.google.com/spreadsheets/d/1naOVRBDOi6G_Amtr8U06ITkPpGE9vQC9/edit?usp=sharing&ouid=116033080101945418210&rtpof=true&sd=true", tab: true },
         { text: "Budget", link: "/budget", tab: false },
+        { text: "Site Updater", link: "/site-updater", tab: false },
         { text: "Game Planner", link: "/game-planner", tab: false },
 
     ]
